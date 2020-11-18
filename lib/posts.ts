2a705de7c346f1179fd3e75b5e6a2df5dd901d27
nurlimandiara <nurlimandiara@gmail.com>
2020-11-18 21:47:50 +0700

    fix error when posts folder containt folder

--- lib/posts.ts
+++ lib/posts.ts
@@ -8,24 +8,29 @@ const postsDirectory = path.join(process.cwd(), "posts");
 
 export function getSortedPostsData() {
   // Get file names under /posts
-  const fileNames = fs.readdirSync(postsDirectory);
-  const allPostsData = fileNames.map((fileName) => {
-    // Remove ".md" from file name to get id
-    const id = fileName.replace(/\.md$/, "");
+  const files = fs.readdirSync(postsDirectory);
+  const allPostsData = files
+    .map((fileName) => {
+      const fileFullPath = path.join(postsDirectory, fileName);
+      const stat = fs.lstatSync(fileFullPath);
 
-    // Read markdown file as string
-    const fullPath = path.join(postsDirectory, fileName);
-    const fileContents = fs.readFileSync(fullPath, "utf8");
+      if (stat.isDirectory()) return null;
+      // Remove ".md" from file name to get id
+      const id = fileName.replace(/\.md$/, "");
 
-    // Use gray-matter to parse the post metadata section
-    const matterResult = matter(fileContents);
+      // Read markdown file as string
+      const fileContents = fs.readFileSync(fileFullPath, "utf8");
 
-    // Combine the data with the id
-    return {
-      id,
-      ...(matterResult.data as { date: string; title: string }),
-    };
-  });
+      // Use gray-matter to parse the post metadata section
+      const matterResult = matter(fileContents);
+
+      // Combine the data with the id
+      return {
+        id,
+        ...(matterResult.data as { date: string; title: string }),
+      };
+    })
+    .filter((item) => Boolean(item));
   // Sort posts by date
   return allPostsData.sort((a, b) => {
     if (a.date < b.date) {
@@ -38,13 +43,20 @@ export function getSortedPostsData() {
 
 export function getAllPostIds() {
   const fileNames = fs.readdirSync(postsDirectory);
-  return fileNames.map((fileName) => {
-    return {
-      params: {
-        id: fileName.replace(/\.md$/, ""),
-      },
-    };
-  });
+  return fileNames
+    .map((fileName) => {
+      const fileFullPath = path.join(postsDirectory, fileName);
+      const stat = fs.lstatSync(fileFullPath);
+
+      if (stat.isFile()) {
+        return {
+          params: {
+            id: fileName.replace(/\.md$/, ""),
+          },
+        };
+      } else return null;
+    })
+    .filter((item) => Boolean(item));
 }
 
 export async function getPostData(id: string) {
